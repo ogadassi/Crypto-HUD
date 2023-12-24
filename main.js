1161cdad307bf013c2179399dea67b797090c17e
ogadassi <ogadassi@gmail.com>
2023-12-24 18:09:14 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,190 @@
+"use strict";
+$(() => {
+  createHome();
+  $("#homeLink").click(() => createHome());
+  $("#reportsLink").click(() => createReports());
+  $("#aboutLink").click(() => createAbout());
+
+  //   pages creation
+  async function createHome() {
+    const coins = await getJson("assets/jsons/coins.json");
+    displayCoins(coins);
+    getRandomNewsItem();
+  }
+  function createReports() {
+    $("#container").html("Reports");
+  }
+  function createAbout() {
+    $("#container").html("About");
+  }
+
+  //   search function
+  $("#searchBox").on("input", async () => {
+    const searchWord = $("#searchBox").val();
+    let matchingCoins = [];
+    const coins = await getJson("assets/jsons/coins.json");
+    for (const coin of coins) {
+      if (
+        coin.id.includes(searchWord) ||
+        coin.symbol.includes(searchWord) ||
+        coin.name.includes(searchWord)
+      ) {
+        matchingCoins.push(coin);
+      }
+    }
+    if (matchingCoins.length >= 0) {
+      displayCoins(matchingCoins);
+    }
+  });
+
+  //   coin display
+  function displayCoins(coins) {
+    let content = "";
+    let count = 0;
+    for (const coin of coins) {
+      const div = `<div class="card">
+      <button class="checkbox ${coin.symbol}">button</button>
+        <div><img src="${coin.image.small}"></div>
+        <div>${coin.symbol}</div>
+        <div>${coin.name}</div>
+        <button id="btn${count}" class='moreInfo' data-coin-id="${coin.id}">More Info</button>
+        <div class="more-info hidden">
+          more info
+        </div>
+      </div>`;
+      content += div;
+      count++;
+    }
+
+    $("#container").html(content);
+
+    const infoButtons = document.querySelectorAll(".card > .moreInfo");
+    for (const button of infoButtons) {
+      button.addEventListener("click", toggleMoreInfo);
+    }
+  }
+  let chosenButtonsArr = [];
+
+  $("#container").on("click", "button.checkbox", function () {
+    this.classList.toggle("checked");
+
+    if (this.classList.contains("checked")) {
+      if (chosenButtonsArr.indexOf(this) === -1) {
+        if (chosenButtonsArr.length < 5) {
+          chosenButtonsArr.push(this);
+        } else {
+          getCoinsFromButtons(chosenButtonsArr);
+          this.classList.remove("checked");
+        }
+      }
+    } else {
+      const index = chosenButtonsArr.indexOf(this);
+      if (index !== -1) {
+        chosenButtonsArr.splice(index, 1);
+      }
+    }
+  });
+
+  let chosenCoinsArr = [];
+  async function getCoinsFromButtons(arr) {
+    const coins = await getJson("assets/jsons/coins.json");
+
+    chosenCoinsArr = [];
+
+    for (const coin of coins) {
+      for (const button of arr) {
+        if (coin.symbol === button.classList[1] && chosenCoinsArr.length < 5) {
+          chosenCoinsArr.push(coin);
+        }
+      }
+    }
+
+    showModal();
+  }
+
+  function showModal() {
+    let content = `<div id="modal">`;
+    console.log(chosenCoinsArr);
+    content += `<button id="modalExit">x</button>`;
+    for (const coin of chosenCoinsArr) {
+      content += `<div class="modalCard">
+          <div><img src="${coin.image.small}"></div>
+          <div>${coin.symbol}</div>
+          <div>${coin.name}</div>
+          <button class="removeBtn ${coin.name}">remove</button>
+          </div>`;
+    }
+    content += `</div>`;
+    $("#modalContainer").html(content);
+  }
+
+  $("#modalContainer").on("click", "#modalExit", () => hideModal());
+
+  $("#modalContainer").on("click", ".removeBtn", function () {
+    const coinNameToRemove = this.classList[1];
+
+    const indexToRemove = chosenCoinsArr.findIndex(
+      (coin) => coin.name === coinNameToRemove
+    );
+
+    if (indexToRemove !== -1) {
+      chosenCoinsArr.splice(indexToRemove, 1);
+    }
+
+    hideModal();
+
+    $(".checkbox." + coinNameToRemove).removeClass("checked");
+  });
+
+  function hideModal() {
+    $("#modal").fadeOut(500, function () {
+      // Retrieve the coinNameToRemove from the data attribute
+      const coinNameToRemove = $("#modalExit").data("coinNameToRemove");
+
+      // Remove the "checked" class from the corresponding button
+      $(".checkbox." + coinNameToRemove).removeClass("checked");
+    });
+  }
+
+  async function toggleMoreInfo() {
+    const coinId = this.getAttribute("data-coin-id");
+    const prices = await getMoreInfo(coinId);
+    const div = document.querySelector(
+      `button[data-coin-id="${coinId}"] + div`
+    );
+    div.innerHTML = `
+    <div>USD: $${prices.usd} </div>
+    <div>EUR: €${prices.eur} </div>
+    <div>ILS: ₪${prices.ils} </div>`;
+
+    div.classList.toggle("hidden");
+  }
+  //   -----------------------------------------------------------------------
+  async function getMoreInfo(coinId) {
+    let prices = JSON.parse(localStorage.getItem(coinId));
+    if (prices) return prices;
+    const url = "https://api.coingecko.com/api/v3/coins/" + coinId;
+    const coinInfo = await getJson(url);
+    const usd = coinInfo.market_data.current_price.usd;
+    const eur = coinInfo.market_data.current_price.eur;
+    const ils = coinInfo.market_data.current_price.ils;
+    prices = { usd, eur, ils };
+    localStorage.setItem(coinId, JSON.stringify(prices));
+    return prices;
+  }
+
+  async function getRandomNewsItem() {
+    const news = await getJson("assets/jsons/news.json");
+    setInterval(() => {
+      $(".ticker__item").text(
+        news.headlines[Math.floor(Math.random() * 29)].title
+      );
+    }, 14500);
+  }
+
+  async function getJson(url) {
+    const response = await fetch(url);
+    const json = response.json();
+    return json;
+  }
+});
